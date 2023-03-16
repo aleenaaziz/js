19418c022a3ed87b6446e40e36bee18519f05692
aleenaaziz <120034981+aleenaaziz@users.noreply.github.com>
2023-03-16 21:03:38 +0500

    Add files via upload

---
+++ numbers.js
@@ -0,0 +1,14 @@
+let newAge = 20;
+alert("i am " + age + "year old");
+
+let visited = 14 ;
+alert("You hava visited this site" + 14 + "times");
+
+let birthYear = 2002;
+document.write('<br>' + "my birth year is" + birthYear + '<br>' + "Data type of my declared variable is number");
+
+let store = "daraz";
+let visitor = "Saad";
+let product = "5 T-shirt(s)";
+
+document.write('<br>' + visitor + "" + "orderd" + "" + product + "" + "from" + "" + store + "" + "store");
